7733b848d04e59b4ee820356e825fa7d631289e5
dankoBo <krekhovecjob@gmail.com>
2024-03-20 14:31:32 +0200

    implement logic for Form, Redux, bug fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { AppWrapper, GlobalStyle } from './App.styled'
 import Hero from './layout/hero/Hero'
-import Logo from './components/logo/Logo'
 import Content from './layout/content/Content'
 
 function App() {
-
     return (
         <>
             <GlobalStyle />
             <AppWrapper>
                 <Hero />
-                {/* <Logo /> */}
                 <Content />
             </AppWrapper>
         </>
--- src/layout/content/Content.tsx
+++ src/layout/content/Content.tsx
@@ -1,9 +1,6 @@
 import { S_Container, S_NavContainer, S_MainContent } from './Content.styled'
 import Navigtion from "../../components/navigation/Navigation";
-// import GamesCards from '../../components/cards/games-cards/GamesCards';
 import GameInfo from '../../components/game-info/GameInfo';
-import LoginForm from '../../components/login-form/LoginForm';
-
 const Content = () => {
     return (
         <S_Container>
@@ -11,8 +8,6 @@ const Content = () => {
                 <Navigtion />
             </S_NavContainer>
             <S_MainContent>
-                <LoginForm />
-                {/* <GamesCards /> */}
                 <GameInfo />
             </S_MainContent>
         </S_Container>
