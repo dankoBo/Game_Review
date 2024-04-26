67202982563ab56f8cece14b4dab4ba8089925ba
dankoBo <krekhovecjob@gmail.com>
2024-04-26 17:15:25 +0300

    changing: pagination, gameInfo

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,4 +1,3 @@
-// import GameCard from '../cards/games-cards/game-card/GameCard';
 import LoginForm from '../login-form/LoginForm';
 import { S_Container, S_Twinkling } from './Banner.styled';
 import Moon from './moon/Moon';
--- src/components/banner/moon/login-btn/LoginBtn.styled.tsx
+++ src/components/banner/moon/login-btn/LoginBtn.styled.tsx
@@ -9,7 +9,6 @@ const S_Button = styled.button`
     border: none;
     background-color: rgba(162, 162, 162, 0.9);
     box-shadow: 1px 0 0 1px rgba(98, 98, 98, 0.51);
-    cursor: pointer;
 `
 
 export { S_Button };
