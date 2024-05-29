a623e9926ca56b8f727ac70a785bb45186179f27
dankoBo <krekhovecjob@gmail.com>
2024-05-29 16:05:40 +0300

    refactoring GamesCards, changing search logic

--- src/components/cards/GamesCards.tsx
+++ src/components/cards/GamesCards.tsx
@@ -19,8 +19,6 @@ type GameCardProps = {
 };
 
 const GamesCards: FC<GameCardProps> = ({ searchTerm}) => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [currentPage, setCurrentPage] = useState(1);
     const gamesPerPage = 6;
     
     const formatReview = (review: string) => {
@@ -31,36 +29,7 @@ const GamesCards: FC<GameCardProps> = ({ searchTerm}) => {
             </React.Fragment>
         ));
     };
-
-    useEffect(() => {
-        const db = getFirestore(app);
-        const gamesCollection = collection(db, 'games');
-
-        const unsubscribe = onSnapshot(gamesCollection, (querySnapshot) => {
-            const gamesData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() } as Game));
-            setGames(gamesData);
-        });
-
-        // функція, яка буде викликана при видаленні компонента
-        return () => {
-            unsubscribe();
-        };
-    }, []);
-
-    const filteredGames = games.filter(game => 
-        game.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-
-    const paginatedGames = filteredGames.slice((currentPage - 1) * gamesPerPage, currentPage * gamesPerPage);
-
-    const handlePageChange = (direction: 'next' | 'prev') => {
-        if (direction === 'next' && currentPage < Math.ceil(games.length / gamesPerPage)) {
-            setCurrentPage(currentPage + 1);
-        } else if (direction === 'prev' && currentPage > 1) {
-            setCurrentPage(currentPage - 1);
-        }
-    };
-
+    
     return (
         <>
             <S_Container>
--- src/layout/content/Content.styled.tsx
+++ src/layout/content/Content.styled.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 const S_Container = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
     padding: 0 10px;
 `
 
