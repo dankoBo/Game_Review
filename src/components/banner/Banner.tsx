af7db40a3dc384764aee5fc5389e5a7b04c37c5a
dankoBo <b.mazur.dev@gmail.com>
2025-03-06 13:21:01 +0200

    moving GameInfo to content

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,20 +1,16 @@
-import GameInfo from '@/components/game-info/GameInfo';
 import Moon from '@/components/banner/moon/Moon';
 import LoginForm from '@/components/login-form/LoginForm';
 import AdminControls from '../admin-controls/AdminControls';
 import { useLogin } from '@/store/login.store';
-import { useGameInfo } from '@/store/game-info.store';
 import { useAdminPanel } from '@/store/admin-panel.store';
 import { S_BannerContainer, S_BannerTwinkling } from '@/components/banner/Banner.styled';
 
 const Banner = () => {
     const { isLoginOpen } = useLogin();
-    const { isGameInfoOpen } = useGameInfo();
     const { isAdminPanelOpen } = useAdminPanel();
 
     return (
         <S_BannerContainer>
-            {isGameInfoOpen && <GameInfo />}
             <S_BannerTwinkling>
                 <Moon />
                 {isLoginOpen && <LoginForm />}
